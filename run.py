19e207f9ce49d7e42fe689451c8530f40166ec22
SP Mohanty <spmohanty91@gmail.com>
2019-01-04 18:15:25 +0100

    Add proposed submission template code

--- run.py
+++ run.py
@@ -1,7 +1,32 @@
 from obstacle_tower_env import ObstacleTowerEnv
 import sys
 
+def run_evaluation(env):
+    """
+    Runs an arbitrary number of episodes during evaluation
+    """
+    while True :
+        obs = env.reset()
+        if obs == False:
+            """
+                The evaluator returns observation==False after it has run
+                the required number of episodes. If it returns a valid
+                observation on an `env.reset` call, then the evaluator expects
+                to run another episode.
+            """
+            break
+        done = False
+        reward = 0.0
+
+        while not done:
+            action = env.action_space.sample()
+            obs, reward, done, info = env.step(action)
+        return reward
+
 def run_episode(env):
+    """
+    Runs a single episode during local debug
+    """
     env.reset()
     done = False
     reward = 0.0
@@ -18,11 +43,10 @@ if __name__ == '__main__':
 
     env = ObstacleTowerEnv(environment_filename)
     if env.is_grading():
-        episode_reward = run_episode(env)
+        run_evaluation(env)
     else:
         while True:
             episode_reward = run_episode(env)
             print("Episode reward: " + str(episode_reward))
 
     env.close()
-
