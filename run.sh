99c68faf2ed0f01ee8bc3e411bbdd4e85484a733
SP Mohanty <spmohanty91@gmail.com>
2019-06-18 16:07:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Use This line would work in case of the docker container
-# Else the relevant env variables will not be loaded during evaluatino
-source activate base
-echo "root"
-
 python run.py 
